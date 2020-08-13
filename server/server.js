1dffd4b74ad969ba7922bc549397772badc15356
Christian Jaena <christian.jaena@msugensan.edu.ph>
2020-08-13 20:37:45 +0800

    initial commit

---
+++ server/server.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+const pool = require('./dbConnection');
+
+// * MIDDLEWARES //
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+// * ROUTES //
+
+// TODO: create a todo //
+
+app.post('/todos', async (req, res) => {
+	try {
+		const { description } = req.body;
+		const newTodo = await pool.query(
+			'INSERT INTO todos (description) VALUES($1) RETURNING *',
+			[description]
+		);
+		res.json(newTodo.rows[0]);
+	} catch (err) {
+		console.error(err.message);
+	}
+});
+
+// TODO: get all todos //
+
+app.get('/todos', async (req, res) => {
+	try {
+		const allTodos = await pool.query('SELECT * FROM todos');
+		res.json(allTodos.rows);
+	} catch (err) {
+		console.error(err.message);
+	}
+});
+
+// TODO: get a todo //
+
+app.get('/todos/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const todo = await pool.query('SELECT * FROM todos WHERE todo_id = $1', [
+			id,
+		]);
+		res.json(todo.rows[0]);
+	} catch (err) {
+		console.error(err.message);
+	}
+});
+
+// TODO: update a todo //
+
+app.put('/todos/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { description } = req.body;
+		const updateTodo = await pool.query(
+			'UPDATE todos SET description = $1 WHERE todo_id = $2',
+			[description, id]
+		);
+
+		res.json('Todo was updated');
+	} catch (err) {
+		console.error(err.message);
+	}
+});
+
+// TODO: delete a todo //
+
+app.delete('/todos/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const deleteTodo = await pool.query('DELETE FROM todos WHERE todo_id = $1', [
+			id,
+		]);
+
+		res.json('Todo was deleted!');
+	} catch (err) {
+		console.error(err.message);
+	}
+});
+
+// * START SERVER //
+app.listen(5000, () => {
+	console.log('Server running at port 5000');
+});
