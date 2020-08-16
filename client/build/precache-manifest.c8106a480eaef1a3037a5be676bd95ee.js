50521ab1dbde6207bf4a97b3bb6237aaa9b53301
Christian Jaena <christian.jaena@msugensan.edu.ph>
2020-08-16 13:51:48 +0800

    changes for deployment

---
+++ client/build/precache-manifest.c8106a480eaef1a3037a5be676bd95ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27720f9c4cf79452f5a01cfd9d66265e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f859884427ff6ca0f44",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b8707d8e45ae7c88369a",
+    "url": "/static/js/2.fc40807c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc40807c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f859884427ff6ca0f44",
+    "url": "/static/js/main.3b7d843b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
