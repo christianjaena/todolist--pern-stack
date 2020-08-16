ead3dc168a3345c041a1bfe3f772d60a010aaa24
Christian Jaena <christian.jaena@msugensan.edu.ph>
2020-08-16 14:14:32 +0800

    fixed dependency issues in deployment

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
 const app = express();
 const path = require('path');
 const todoRoutes = require('./routes/todoRoutes');
@@ -10,10 +9,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
+} else {
+	const morgan = require('morgan');
+	app.use(morgan('dev'));
 }
 // * ROUTES //
 
@@ -21,8 +22,8 @@ app.use('/todos', todoRoutes);
 
 // ? CATCH-ALL ROUTE
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'client/build/index.html'))
-})
+	res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
 
 app.listen(PORT, () => {
 	console.log(`Server running at port ${PORT}`);
